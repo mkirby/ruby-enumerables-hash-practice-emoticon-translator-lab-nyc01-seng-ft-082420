414879defef62e59d33808a4914bb4e71efbf599
Matthew Kirby <matthew.robbins.kirby@gmail.com>
2020-07-22 14:18:25 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ def get_english_meaning(file_path, emoticon)
   translator = load_library(file_path)
   translator.each do |key, value|
     if value[:japanese] == emoticon
-      return value[:english]
+      return key
     end
   end
 end
