16fdf6bee2dcfdf36854c36d20d09cbc9764ff96
Matthew Kirby <matthew.robbins.kirby@gmail.com>
2020-07-22 14:00:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,6 +5,10 @@ require "pry"
 def load_library(file_path)
   # code goes here
   translator = YAML.load_file(file_path)
+  translator.each do |key, value|
+    puts key
+    puts value
+  end
 end
 
 def get_japanese_emoticon
